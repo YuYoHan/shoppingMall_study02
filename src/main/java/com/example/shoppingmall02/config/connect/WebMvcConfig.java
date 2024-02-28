bd51caa462868f978b1f67be743171eb626f6112
YuYoHan <zxzz8014@naver.com>
2024-02-28 22:27:48 +0900

    [Add] 다른 서버 연결 설정 추가

--- src/main/java/com/example/shoppingmall02/config/connect/WebMvcConfig.java
+++ src/main/java/com/example/shoppingmall02/config/connect/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.shoppingmall02.config;
+package com.example.shoppingmall02.config.connect;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
