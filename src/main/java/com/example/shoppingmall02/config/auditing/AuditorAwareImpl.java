58ebe5e8c6303e1e1874c41415f33ce2192269d8
YuYoHan <zxzz8014@naver.com>
2024-02-28 22:23:59 +0900

    [Feat] Auditing 기능 구현

---
+++ src/main/java/com/example/shoppingmall02/config/auditing/AuditorAwareImpl.java
@@ -0,0 +1,23 @@
+package com.example.shoppingmall02.config.auditing;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        // SecurityContextHodler에  저장된 값을 가져온다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String email = "";
+        if(authentication != null) {
+            // 여기서 getName은 principalService에 이메일로 설정하면 그걸 가져온다.
+            email = authentication.getName();
+        }
+        return Optional.of(email);
+    }
+}
