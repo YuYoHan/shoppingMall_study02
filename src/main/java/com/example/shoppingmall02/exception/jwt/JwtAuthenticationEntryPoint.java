0f3ae900b0b4be91837b8c40c1fa5e57083123d4
YuYoHan <zxzz8014@naver.com>
2024-03-02 20:44:26 +0900

    [Feat] JWT 예외처리 기능 구현

---
+++ src/main/java/com/example/shoppingmall02/exception/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.example.shoppingmall02.exception.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+ *   writer : YuYoHan
+ *   work :
+ *          JWT를 통한 인증이 실패할 때 호출되는 역할입니다.
+ *   date : 2024/01/17
+ * */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증에 실패했습니다.");
+    }
+}
