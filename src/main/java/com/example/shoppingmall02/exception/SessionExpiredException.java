5c869dadda6b553e79c65c520a40bfa7a798e290
YuYoHan <zxzz8014@naver.com>
2024-04-09 12:48:54 +0900

    [Feat] 예외처리 기능 구현

---
+++ src/main/java/com/example/shoppingmall02/exception/SessionExpiredException.java
@@ -0,0 +1,12 @@
+package com.example.shoppingmall02.exception;
+/*
+ *   writer : 유요한
+ *   work :
+ *          인증 예외처리
+ *   date : 2023/10/12
+ * */
+public class SessionExpiredException extends RuntimeException{
+    public SessionExpiredException(String message) {
+        super(message);
+    }
+}
